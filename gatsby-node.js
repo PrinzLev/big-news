5074d48886ab2bf4546de070d8c648807a59d537
Jericho Irvin <aldejerichoirvin@gmail.com>
2021-01-31 21:58:13 +0800

    window variable on SSR

--- gatsby-node.js
+++ gatsby-node.js
@@ -47,3 +47,17 @@ exports.createPages = async ({ graphql, actions }) => {
     })
   })
 }
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /react-particle-animation/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}
