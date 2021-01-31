51262c34d4ff71ab56bb817b3f343dc12fbd9c42
Jericho Irvin <aldejerichoirvin@gmail.com>
2021-01-31 22:20:42 +0800

    windows dummy rendering due to bad module requirement

--- gatsby-node.js
+++ gatsby-node.js
@@ -47,3 +47,17 @@ exports.createPages = async ({ graphql, actions }) => {
     })
   })
 }
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /tiny-slider-react/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}
